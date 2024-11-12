aa22274d0ef0be6a342cb475f768e5604836d16d
JuanAntonioDuran <jdurfer0405@iescarrillo.es>
2024-11-12 19:46:41 +0100

    Creacion de los componentes Ejercicio 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { TasklistComponent } from './components/task/tasklist/tasklist.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,LoginComponent,RegisterComponent,TasklistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

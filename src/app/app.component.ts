22fe96cfa981366cc4c02c5cd4e298cea9150e9b
JuanAntonioDuran <jdurfer0405@iescarrillo.es>
2024-11-18 14:31:28 +0100

    Comit obligatorio Clase 18/11/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { SiginComponent } from './components/auth/sigin/sigin.component';
 import { LoginComponent } from './components/auth/login/login.component';
-import { RegisterComponent } from './components/auth/register/register.component';
+import { TaskComponent } from './components/task/task.component';
+import { ResumeComponent } from './components/task/resume/resume.component';
+import { TaskformComponent } from './components/task/taskform/taskform.component';
 import { TasklistComponent } from './components/task/tasklist/tasklist.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,FooterComponent,LoginComponent,RegisterComponent,TasklistComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, AuthComponent, SiginComponent, LoginComponent, TaskComponent, ResumeComponent, TaskformComponent, TasklistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-tutorial';
+
+  randomInt():number{
+    return Math.random()*10;
+  }
 }

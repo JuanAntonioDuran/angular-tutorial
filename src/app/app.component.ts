3dfeadbfc74b3f73c5cfc9b82d1651c4ff93e4ed
JuanAntonioDuran <jdurfer0405@iescarrillo.es>
2024-11-19 20:28:23 +0100

    Modificacion para funcionamiento del workflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ import { TasklistComponent } from './components/task/tasklist/tasklist.component
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  title = 'angular-tutorial-app';
 
   randomInt():number{
     return Math.random()*10;

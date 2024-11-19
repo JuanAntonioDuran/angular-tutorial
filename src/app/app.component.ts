358608a35f2a7d3e75470f876687eb8f0af2de97
JuanAntonioDuran <jdurfer0405@iescarrillo.es>
2024-11-19 21:08:03 +0100

    Modificacacion tras runeo de build:prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { SiginComponent } from './components/auth/sigin/sigin.component';
 import { LoginComponent } from './components/auth/login/login.component';
-import { TaskComponent } from './components/task/task.component';
 import { ResumeComponent } from './components/task/resume/resume.component';
 import { TaskformComponent } from './components/task/taskform/taskform.component';
 import { TasklistComponent } from './components/task/tasklist/tasklist.component';
@@ -13,7 +12,7 @@ import { TasklistComponent } from './components/task/tasklist/tasklist.component
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, AuthComponent, SiginComponent, LoginComponent, TaskComponent, ResumeComponent, TaskformComponent, TasklistComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, AuthComponent, SiginComponent, LoginComponent, ResumeComponent, TaskformComponent, TasklistComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ResumeComponent } from './components/task/resume/resume.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {path:'navbar', component:NavbarComponent},
+    {path:'resume', component:ResumeComponent},
+    {path:'', redirectTo:'/navbar', pathMatch:'full'}
+
+];
